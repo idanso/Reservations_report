9692ec1ca83351e5af360442f7562d468dc4a298
idanso <sorany123@gmail.com>
2021-11-02 16:33:01 +0200

    - first commit

---
+++ Email_send_report.py
@@ -0,0 +1,34 @@
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+contacts = ['idanso@radware.com']
+
+msg = EmailMessage()
+msg['Subject'] = 'DemoLabs Reservations Monthly Report'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = contacts
+
+msg.set_content('Users reservations summary file attached')
+
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color:SlateGray;">This is an HTML Email!</h1>
+    </body>
+</html>
+""", subtype='html')
+
+with smtplib.SMTP_SSL('smtp address') as smtp:
+    smtp.send_message(msg)
+    smtp.quit()
+
+# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+#     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+#     smtp.send_message(msg)
